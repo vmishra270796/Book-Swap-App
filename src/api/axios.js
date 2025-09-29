9297eea5b95d6c9cfaa930ca79e4fef6ef6bd376
Vikram <vikrammishra270796@gmail.com>
2025-09-29 10:38:50 +0530

    added env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
-  withCredentials: true
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
